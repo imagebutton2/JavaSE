75a3c670d13f1e0b82251775adaf9285da4402d1
imagebutton2 <1046509851@qq.com>
2019-03-18 20:47:53 +0800

    Lambda表达式方法引用构造方法

---
+++ TestDemo4/TestDemo.java
@@ -0,0 +1,58 @@
+﻿//Lambda表达式方法引用
+//4.引用构造方法
+interface IUi<R，P1，P2>{
+
+    R createPerson(P1 p1,P2 p2);
+   
+}
+class Person{
+    private String name ;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        super();
+        this.name = name;
+        this.age = age;
+    }
+}
+
+
+
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+      
+        //引用Person的构造方法
+
+        IUi<Person,String,Integer>iUi=Person::new;
+
+        System.out.println(iUi.createPerson("libao", 10));
+
+        
+    }
+}
