995002c68d30d5247b298d715346d0fde8093be5
imagebutton2 <1046509851@qq.com>
2019-04-12 23:09:24 +0800

    //创建一个新文件

---
+++ newFile/newFile.java
@@ -0,0 +1,21 @@
+﻿//创建一个新文件
+public class TestFile {
+
+    public static void main(String[] args) {
+        File file = new File("D:\\Test\\java.java");
+        //File file = new File("D:\\Test","java.java");
+        boolean rs = false;
+        try {
+            rs=file.createNewFile();//创建一个新文件
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(rs){
+            System.out.println("文件创建成功");
+        }else {
+            System.out.println("文件创建失败");
+        }
+
+    }
+}
