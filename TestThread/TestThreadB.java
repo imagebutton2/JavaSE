17cdf5192e41e904870671d15f1803f0c8789502
imagebutton2 <1046509851@qq.com>
2019-04-16 21:04:53 +0800

    多线程的Test

---
+++ TestThread/TestThreadB.java
@@ -0,0 +1,36 @@
+
+
+public class TestThreadB {
+    
+    public static void main(String[] args) {
+        MyRunnable myRunnableA = new MyRunnable("MyRunnable-ThreadA");
+        MyRunnable myRunnableB = new MyRunnable("MyRunnable-ThreadB");
+        
+        
+        Thread threadA = new Thread(myRunnableA);
+        Thread threadB = new Thread(myRunnableB);
+        
+        System.out.println("TestThreadB-Main-Method Before");
+        threadA.start();
+        threadB.start();
+        System.out.println("TestThreadB-Main-Method After");
+        
+    }
+    
+}
+
+class MyRunnable implements Runnable {
+    
+    private String title;
+    
+    MyRunnable(String title) {
+        this.title = title;
+    }
+    
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(this.title + " " + i);
+        }
+    }
+}
