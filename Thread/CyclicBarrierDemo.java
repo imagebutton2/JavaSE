d39ec7be2f5b9b391dea6c6713bf080ad3f78d9f
imagebutton2 <1046509851@qq.com>
2019-08-10 22:18:40 +0800

    CyclicBarrier 简单实现

---
+++ Thread/CyclicBarrierDemo.java
@@ -0,0 +1,24 @@
+package com.gjz.Juc;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> System.out.println("召唤神龙"));
+        for (int i = 1; i <= 7; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "\t" + tempInt);
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+                    , String.valueOf(tempInt)).start();
+        }
+    }
+}
